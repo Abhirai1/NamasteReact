b489a88edee183fca88f4478abaa1b76d7ac3100
Abhishek Rai <abhirai2250@gmail.com>
2024-05-04 09:16:55 +0530

    third lecture

--- app.js
+++ app.js
@@ -2,16 +2,36 @@
  * 
  * HMR - Hot module reloading
  * 
+ * File watcher algorithm (jo bhi ham chnages karte hai vo reflect ho pata hai iske karan se)
+ * FWA is written in C++
  * 
  * 
+ * Parcel is beast(here is why):-
+ * Building
+ * Mininfy
+ * Dev and Production build
+ * Super fast build algortihm
+ * Image optimisation
+ * Caching while development
+ * compression
+ * comnpitable with older version of the browser
+ * HTTPS on dev
  * 
  * 
+ * we should push parcel-catch in git ignore 
+ * anything we can generate from server we push it to our server
  */
 
 const React=require('react');
 const ReactDOM=require('react-dom/client');
 
-const heading = React.createElement("h1",{},"Hello World!");
+const heading = React.createElement(
+    "h1",
+    {
+        id:"heading"
+    },
+    "Hello World!"
+);
 const root = document.getElementById('root');
 
 ReactDOM.createRoot(root).render(heading);
--- dist/index.5baa4167.js
+++ dist/index.5baa4167.js
@@ -2955,13 +2955,29 @@ try {
  * 
  * HMR - Hot module reloading
  * 
+ * File watcher algorithm (jo bhi ham chnages karte hai vo reflect ho pata hai iske karan se)
+ * FWA is written in C++
  * 
  * 
+ * Parcel is beast(here is why):-
+ * Building
+ * Mininfy
+ * Dev and Production build
+ * Super fast build algortihm
+ * Image optimisation
+ * Caching while development
+ * compression
+ * comnpitable with older version of the browser
+ * HTTPS on dev
  * 
  * 
+ * we should push parcel-catch in git ignore 
+ * anything we can generate from server we push it to our server
  */ const React = require("53fc6a5398fc4b1");
 const ReactDOM = require("b1b8b98febfa23fa");
-const heading = React.createElement("h1", {}, "Hello World!");
+const heading = React.createElement("h1", {
+    id: "heading"
+}, "Hello World!");
 const root = document.getElementById("root");
 ReactDOM.createRoot(root).render(heading);
 
