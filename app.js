b10b664b0fc09a636b4fe8ad319acec30a584e1a
Abhishek Rai <abhirai2250@gmail.com>
2024-05-04 10:34:12 +0530

    third lecture updated

--- app.js
+++ app.js
@@ -7,6 +7,13 @@
  * 
  * 
  * Parcel is beast(here is why):-
+ * akel ye sab nhi karta ye baki ke package ke help se ye sab kar pata hai 
+ * node module me jao parcel open kro 
+ * 
+ * word-> this is known as transitive dependencies 
+ * depend->depend->depend->depend
+ * 
+ * 
  * Building
  * Mininfy
  * Dev and Production build
@@ -16,10 +23,14 @@
  * compression
  * comnpitable with older version of the browser
  * HTTPS on dev
+ * port number
+ * consitent hashing algorithm
+ * zero config
  * 
  * 
  * we should push parcel-catch in git ignore 
  * anything we can generate from server we push it to our server
+ * 
  */
 
 const React=require('react');
