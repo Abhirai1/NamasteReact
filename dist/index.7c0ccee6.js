a47500d2b15e1ebde64980be03f985b355427dfb
Abhishek Rai <101519306+Abhirai1@users.noreply.github.com>
2024-05-05 23:14:13 +0530

    Delete dist directory

--- dist/index.7c0ccee6.js
+++
@@ -1,6 +0,0 @@
-const React = require();
-const heading = React.createElement("h1", {}, "hari bol abhi!");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
-
-//# sourceMappingURL=index.7c0ccee6.js.map
